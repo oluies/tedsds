03a915a8fe7b602c828b7c2a49843710bfcc945c
oluies <orjan.lundberg@gmail.com>
2016-03-09 16:55:57 +0100

    metrics

--- src/main/scala/com/combient/sparkjob/tedsds/MulticlassMetrics.scala
+++ src/main/scala/com/combient/sparkjob/tedsds/MulticlassMetrics.scala
@@ -110,44 +110,49 @@ object MulticlassMetricsFortedsds {
     val metrics = new MulticlassMetrics(predictionAndLabels)
 
     // Confusion matrix
-    println("Confusion matrix:")
-    println(metrics.confusionMatrix)
+    val metricsStrb  = new StringBuilder(1024);
+    metricsStrb append "Confusion matrix:"
+    metricsStrb append metrics.confusionMatrix
 
     // Overall Statistics
     val precision = metrics.precision
     val recall = metrics.recall // same as true positive rate
     val f1Score = metrics.fMeasure
-    println("Summary Statistics")
-    println(s"Precision = $precision")
-    println(s"Recall = $recall")
-    println(s"F1 Score = $f1Score")
+    metricsStrb append "\nSummary Statistics"
+    metricsStrb append s"\nPrecision = $precision"
+    metricsStrb append s"\nRecall = $recall"
+    metricsStrb append s"\nF1 Score = $f1Score"
 
     // Precision by label
     val labels = metrics.labels
     labels.foreach { l =>
-      println(s"Precision($l) = " + metrics.precision(l))
+      metricsStrb append (s"\nPrecision($l) = " + metrics.precision(l))
     }
 
     // Recall by label
     labels.foreach { l =>
-      println(s"Recall($l) = " + metrics.recall(l))
+      metricsStrb append (s"\nRecall($l) = " + metrics.recall(l))
     }
 
     // False positive rate by label
     labels.foreach { l =>
-      println(s"FPR($l) = " + metrics.falsePositiveRate(l))
+      metricsStrb append (s"\nFPR($l) = " + metrics.falsePositiveRate(l))
     }
 
     // F-measure by label
     labels.foreach { l =>
-      println(s"F1-Score($l) = " + metrics.fMeasure(l))
+      metricsStrb append (s"\nF1-Score($l) = " + metrics.fMeasure(l))
     }
 
     // Weighted stats
-    println(s"Weighted precision: ${metrics.weightedPrecision}")
-    println(s"Weighted recall: ${metrics.weightedRecall}")
-    println(s"Weighted F1 score: ${metrics.weightedFMeasure}")
-    println(s"Weighted false positive rate: ${metrics.weightedFalsePositiveRate}")
+    metricsStrb append (s"\nWeighted precision: ${metrics.weightedPrecision}")
+    metricsStrb append (s"\nWeighted recall: ${metrics.weightedRecall}")
+    metricsStrb append (s"\nWeighted F1 score: ${metrics.weightedFMeasure}")
+    metricsStrb append (s"\nWeighted false positive rate: ${metrics.weightedFalsePositiveRate}")
+    println("***")
+    println(metricsStrb.toString())
+
+    sc.stop()
   }
 }
 // scalastyle:on println
