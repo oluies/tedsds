d5961b0f63e123a797ae3f0787e3dbf940b89f31
oluies <orjan.lundberg@gmail.com>
2016-03-12 19:56:27 +0100

    hdfscript

---
+++ script/hadoopfs.sh
@@ -0,0 +1,13 @@
+hadoop fs -put test_FD001.txt /share/tedsds/input/
+hadoop fs -put test_FD002.txt /share/tedsds/input/
+hadoop fs -put test_FD003.txt /share/tedsds/input/
+hadoop fs -put test_FD004.txt /share/tedsds/input/
+hadoop fs -put train_FD001.txt /share/tedsds/input/
+hadoop fs -put train_FD002.txt /share/tedsds/input/
+hadoop fs -put train_FD003.txt /share/tedsds/input/
+hadoop fs -put train_FD004.txt /share/tedsds/input/
+hadoop fs -put  RUL_FD001.txt  /share/tedsds/input/
+hadoop fs -put  RUL_FD002.txt  /share/tedsds/input/
+hadoop fs -put  RUL_FD003.txt  /share/tedsds/input/
+hadoop fs -put  RUL_FD004.txt  /share/tedsds/input/
+hadoop fs -ls /share/tedsds/input/
