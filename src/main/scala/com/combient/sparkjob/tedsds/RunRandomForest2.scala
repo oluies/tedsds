98d365d1bb86ca242ac602f71432093232948a0e
oluies <orjan.lundberg@gmail.com>
2016-04-07 15:13:15 +0200

    numClasses

--- src/main/scala/com/combient/sparkjob/tedsds/RunRandomForest2.scala
+++ src/main/scala/com/combient/sparkjob/tedsds/RunRandomForest2.scala
@@ -105,9 +105,9 @@ object RunRandomForest2 {
 
     // Train a RandomForest model.
     // Empty categoricalFeaturesInfo indicates all features are continuous.
-    val numClasses = 3 // 0-2
+    val numClasses = 4 // 0-2
     val categoricalFeaturesInfo = Map[Int, Int]()
-    val numTrees = 3 // Use more in practice.
+    val numTrees = 9 // Use more in practice.
     val featureSubsetStrategy = "auto" // Let the algorithm choose.
     val impurity = "gini"
     val maxDepth = 4
