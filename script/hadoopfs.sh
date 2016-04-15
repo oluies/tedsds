36c61e987fe2188efea82ebf07f2b012e9833cc8
Nicolas Innocenti <nicolas.innocenti@combient.com>
2016-04-15 13:55:35 +0200

    Adapted to work on Hortonworks Sandbox

--- script/hadoopfs.sh
+++ script/hadoopfs.sh
@@ -2,20 +2,28 @@
 
 #This script uploads the data from the ../data folder to HDFS
 
-gunzip --keep ../data/*.gz
+#gunzip --keep ../data/*.gz
+for i in `ls ../data/*.gz` 
+do 
+	j=`echo $i | sed "s/\.gz//" ` 
+	gunzip -c $i > $j 
+done
 
-hadoop fs -put test_FD001.txt /share/tedsds/input/
-hadoop fs -put test_FD002.txt /share/tedsds/input/
-hadoop fs -put test_FD003.txt /share/tedsds/input/
-hadoop fs -put test_FD004.txt /share/tedsds/input/
-hadoop fs -put train_FD001.txt /share/tedsds/input/
-hadoop fs -put train_FD002.txt /share/tedsds/input/
-hadoop fs -put train_FD003.txt /share/tedsds/input/
-hadoop fs -put train_FD004.txt /share/tedsds/input/
-hadoop fs -put  RUL_FD001.txt  /share/tedsds/input/
-hadoop fs -put  RUL_FD002.txt  /share/tedsds/input/
-hadoop fs -put  RUL_FD003.txt  /share/tedsds/input/
-hadoop fs -put  RUL_FD004.txt  /share/tedsds/input/
+
+#hadoop fs -rm -r -f /share/tedsds
+hadoop fs -mkdir -p /share/tedsds/input/
+hadoop fs -put ../data/test_FD001.txt /share/tedsds/input/
+hadoop fs -put ../data/test_FD002.txt /share/tedsds/input/
+hadoop fs -put ../data/test_FD003.txt /share/tedsds/input/
+hadoop fs -put ../data/test_FD004.txt /share/tedsds/input/
+hadoop fs -put ../data/train_FD001.txt /share/tedsds/input/
+hadoop fs -put ../data/train_FD002.txt /share/tedsds/input/
+hadoop fs -put ../data/train_FD003.txt /share/tedsds/input/
+hadoop fs -put ../data/train_FD004.txt /share/tedsds/input/
+hadoop fs -put  ../data/RUL_FD001.txt  /share/tedsds/input/
+hadoop fs -put  ../data/RUL_FD002.txt  /share/tedsds/input/
+hadoop fs -put  ../data/RUL_FD003.txt  /share/tedsds/input/
+hadoop fs -put  ../data/RUL_FD004.txt  /share/tedsds/input/
 hadoop fs -ls /share/tedsds/input/
 
 rm -rf ../data/*.txt
