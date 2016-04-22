e4309bc229582e566b851b3a167a6a94d5cfdd89
Nicolas Innocenti <nicolas.innocenti@combient.com>
2016-04-22 12:40:59 +0200

    More comments

--- src/main/scala/com/combient/sparkjob/SimpleExample.scala
+++ src/main/scala/com/combient/sparkjob/SimpleExample.scala
@@ -1,5 +1,22 @@
 /*
- * A simple example of a job
+ * A simple example of a Spark job written in Scala.
+ *
+ * This tasks creates a table with arbitrary values,
+ * distributes it over the nodes, writes it to HDFS
+ * in parallel in two CSV and parquet (Spark optimized file format),
+ * and print the number of lines in the table to the console.
+ *
+ *******************************************************
+ *
+ * Compilation: from the top folder (containing src/ and project/), run
+ *    #sbt assembly
+ *
+ * Make sure the files this program is writing to do not exist
+ * (otherwise, Spark will throw an error)
+ *    #hadoop fs -rm -r -f SimpleExample.*
+ *
+ * Submit the job
+ *    #spark-submit --class com.combient.sparkjob.SimpleExample --master yarn ./target/scala-2.10/tedsds-assembly-1.0.jar
  *
  *
  */
@@ -49,8 +66,9 @@ object SimpleExample {
     dft.write.format("com.databricks.spark.csv").option("header", "true").save("SimpleExample.csv")
 
 
-    println("############################## Count: " ${dft.count()} " ############################## ")
+    println(s"############################## Count: ${dft.count()} ############################## ")
 
+    //Terminates the spark context
     sc.stop()
 
   }
