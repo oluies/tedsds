a27fd23bec54f716127d3acdc36b9667f0edc49c
Nicolas Innocenti <nicolas.innocenti@combient.com>
2016-04-22 15:58:16 +0200

    Comments and small changes

--- src/main/scala/com/combient/sparkjob/tedsds/PrepareData.scala
+++ src/main/scala/com/combient/sparkjob/tedsds/PrepareData.scala
@@ -21,6 +21,7 @@ object PrepareData {
 
   case class Params(input: String = null,output: String = null)
 
+  //The main method only parses input parameters and call the method run()
   def main(args: Array[String]) {
     val defaultParams = Params()
 
@@ -38,10 +39,10 @@ object PrepareData {
         """
           |For example, the following command runs this app on a  dataset:
           |
-          | bin/spark-submit --class com.combient.sparkjob.tedsds.PrepareData \
+          | spark-submit --class com.combient.sparkjob.tedsds.PrepareData \
           |  jarfile.jar \
-          |  "/user/zeppelin/pdm001/tmp.83h1YRpXM2/train_FD001.txt" \
-          |  "/share/tedsds/scaledd"
+          |  "/share/tedsds/input/test_FD001.txt" \
+          |  "/share/tedsds/scaleddftest_FD001/"
         """.stripMargin)
     }
 
@@ -54,17 +55,19 @@ object PrepareData {
 
   }
 
+  //Here we do something useful =)
   def run(params: Params) {
+
+    //Create a Spark context
     val conf = new SparkConf().setAppName("PrepareData")
-
-
     val sc = new SparkContext(conf)
     val sqlContext = new HiveContext(sc)
     import sqlContext.implicits._
 
+    //Define the schema for the dataset
     val customSchema = StructType(Seq(
       StructField("id",      IntegerType,nullable = false),
-      StructField("cykle",   IntegerType,nullable =false),
+      StructField("cycle",   IntegerType,nullable =false),
       StructField("setting1",DoubleType,nullable =true),
       StructField("setting2",DoubleType,nullable =true),
       StructField("setting3",DoubleType,nullable =true),
@@ -90,6 +93,7 @@ object PrepareData {
       StructField("s20",     DoubleType,nullable =true),
       StructField("s21",     DoubleType,nullable =true)))
 
+    //Load the .csv file into a dataframe
     val df: DataFrame = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "false")
@@ -99,32 +103,55 @@ object PrepareData {
       .schema(customSchema)
       .load(params.input)
 
-
+    //This tells Spark to always store the data in memory
     df.persist()
 
-    df.show(10)
+    //Print 10 lines of the dataframe to the console
+    //df.show(10)
 
-    // calculate max cykle per id
-    val maxCyclePerId = df.groupBy($"id").agg(max($"cykle").alias("maxcykle"))
 
+    //For each engine ID, find the cycle with the highest value (corresponding to the cycle at which the engine failed)
+    val maxCyclePerId = df.groupBy($"id").agg(max($"cycle").alias("maxcycle"))
+
+    //Show the result
     //maxCyclePerId.show
 
-    // windows for classifcation
 
+    //******************************************************************************
+    //*
+    //*           Labelling the data
+    //*
+    //******************************************************************************
+
+    //Define the values for converting the remaining usefull life (RUL) into labels
     val w1:Int = 30
     val w0:Int = 15
 
+    //Defines the labels for each engine ID and cycle
     val withrul = df.join(maxCyclePerId,"id")
-      .withColumn("rul",maxCyclePerId("maxcykle")-df("cykle")) // Add RUL as maxcycle-currentcycle per row
+      .withColumn("rul",maxCyclePerId("maxcycle")-df("cycle")) // Calculate the RUL for
       .withColumn("label1",when($"rul" <= w1, 1).otherwise(0)) // add label 1
       .withColumn("label2",when($"rul" <= w0, 2).otherwise(when($"rul" <= w1, 1).otherwise(0))) // add label 2 (1 if under w1, 2 if under w0, zero otherwize)
 
+    //*************************************************************************************
+    //*
+    //* Building features (average and standard deviations of sensor values over a window)
+    //*
+    //*************************************************************************************
+
+
+    //Define the windows width
     val windowRange = 5
+
+    // Group the data by engine ID, order by cycle number and consider windowRange lines at a time:
+    //
     // see https://databricks.com/blog/2015/07/15/introducing-window-functions-in-spark-sql.html
     //     http://spark.apache.org/docs/latest/sql-programming-guide.html
     // PARTITION BY id  ORDER BY cykle ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING (5)
-    val w = Window.partitionBy("id").orderBy("cykle").rowsBetween(0, windowRange)
+    val w = Window.partitionBy("id").orderBy("cycle").rowsBetween(0, windowRange)
 
+
+    // Calculate the mean and std  (Can we do better than this ?)
     // see http://spark.apache.org/docs/latest/sql-programming-guide.html
     val x = withrul.select('*,
       mean($"s1").over(w).as("a1"),
@@ -191,10 +218,15 @@ object PrepareData {
       sqrt( sum(pow($"s21" -  mean($"s21").over(w),2)).over(w) / 5).as("sd21")
     )
 
+
+
+
     // filter away columns from
     // these columns had the lowest correlation factor :  "sd11","sd20","sd4","sd12","sd17","sd8","sd15","sd7","sd2","sd3","sd21","setting1","setting2"
-    val columns = x.columns.diff(Seq("id","maxcykle","rul","label1", "label2", "sd11","sd20","sd4",
-      "sd12","sd17","sd8","sd15","sd7","sd2","sd3","sd21","setting2","setting3","s18","s19"))
+    //val columns = x.columns.diff(Seq("id","maxcycle","rul","label1", "label2", "sd11","sd20","sd4",
+    //  "sd12","sd17","sd8","sd15","sd7","sd2","sd3","sd21","setting2","setting3","s18","s19"))
+
+    val columns = x.columns
 
     println(s"assembler these columns to  features vector ${columns.toList}")
     //see https://spark.apache.org/docs/latest/ml-features.html
@@ -211,7 +243,7 @@ object PrepareData {
 
     val withFeatures = assembler.transform(x)
 
-    withFeatures.show(10)
+    //withFeatures.show(10)
 
     val scaledDF =  scaler.fit(withFeatures).transform(withFeatures)
 
@@ -219,6 +251,9 @@ object PrepareData {
     scaledDF.drop("features")
 
     scaledDF.write.mode(SaveMode.Overwrite).parquet(params.output)
+
+    //Optionally save as CSV for debugging purposes
+    scaledDF.write.mode(SaveMode.Overwrite).format("com.databricks.spark.csv").option("header", "true").save(params.output.concat(".csv"))
 
     sc.stop()
   }
