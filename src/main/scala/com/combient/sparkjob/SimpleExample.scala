542d9080b7e5182ac7650a3030f40c494bbfbe89
Nicolas Innocenti <nicolas.innocenti@combient.com>
2016-04-22 12:23:37 +0200

    Comments and small changes

--- src/main/scala/com/combient/sparkjob/SimpleExample.scala
+++ src/main/scala/com/combient/sparkjob/SimpleExample.scala
@@ -1,34 +1,24 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * A simple example of a job
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
-// scalastyle:off println
 package com.combient.sparkjob
 
 import org.apache.spark.sql.SQLContext
 
-// $example off$
 import org.apache.spark.{ SparkConf, SparkContext }
 
 object SimpleExample {
 
   def main(args: Array[String]): Unit = {
+
+    //Create a spark context
     val conf = new SparkConf().setAppName("SimpleExample")
     val sc = new SparkContext(conf)
 
+    //Create some arbitrary data
     val schema = Seq("id", "cykle", "value")
     val data = Seq(
       (1, 1, 1),
@@ -44,16 +34,24 @@ object SimpleExample {
       (2, 5, 1),
       (2, 6, 11))
 
+
+    //This create a "spark dataframe", i.e. a table in the spark formalism, distributed over the nodes.
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
     val dft = sc.parallelize(data).toDF(schema: _*)
 
-    dft.write.parquet("pm.scaledfeatures")
 
-    println(s"Count: ${dft.count()}")
+    //Write the data into parquet format, a file format optimized for storing Spark dataframe
+    dft.write.parquet("SimpleExample.parquet")
+
+    //Write the data into standard CSV format
+    import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType};
+    dft.write.format("com.databricks.spark.csv").option("header", "true").save("SimpleExample.csv")
+
+
+    println("############################## Count: " ${dft.count()} " ############################## ")
 
     sc.stop()
 
   }
 }
-// scalastyle:on println
