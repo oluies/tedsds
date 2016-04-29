5517b37cef5aab0d06905e2a3fdfdfa33c968e6d
Nicolas Innocenti <nicolas.innocenti@combient.com>
2016-04-29 10:22:51 +0200

    Script to learn models

---
+++ script/run_LearnModels.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+#Simple attempt to figure out the relative path to the data
+if [[ $0 == "./script/run_LearnModels.sh" ]]
+then
+	TARGETDIR=./target
+else
+if [[ $0 == "./run_LearnModels.sh" ]]
+then
+	TARGETDIR=../target
+else
+	echo "Please run this script from within the script/ directory"
+	exit
+fi
+fi
+
+
+#Define command, clean HDFS and learn a Logistic Classifier on the 4 training sets
+SUBMIT_COMMAND="spark-submit --class com.combient.sparkjob.tedsds.RunLogisticRegressionWithLBFGS --master yarn $TARGETDIR/scala-2.10/tedsds-assembly-1.0.jar"
+hadoop fs -rm -r -f /share/tedsds/lr_model*
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD001 /share/tedsds/lr_model_FD001
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD002 /share/tedsds/lr_model_FD002
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD003 /share/tedsds/lr_model_FD003
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD004 /share/tedsds/lr_model_FD004
+
+#The same but with a RandomForest Classifier
+SUBMIT_COMMAND="spark-submit --class com.combient.sparkjob.tedsds.RunRandomForest --master yarn $TARGETDIR/scala-2.10/tedsds-assembly-1.0.jar"
+hadoop fs -rm -r -f /share/tedsds/rf_model*
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD001 /share/tedsds/rf_model_FD001
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD002 /share/tedsds/rf_model_FD002
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD003 /share/tedsds/rf_model_FD003
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD004 /share/tedsds/rf_model_FD004
+
+#The same but with a more complex RandomForest Classifier (more  and deeper trees)
+SUBMIT_COMMAND="spark-submit --class com.combient.sparkjob.tedsds.RunRandomForest2 --master yarn $TARGETDIR/scala-2.10/tedsds-assembly-1.0.jar"
+hadoop fs -rm -r -f /share/tedsds/rf2_model*
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD001 /share/tedsds/rf2_model_FD001
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD002 /share/tedsds/rf2_model_FD002
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD003 /share/tedsds/rf2_model_FD003
+$SUBMIT_COMMAND  /share/tedsds/scaleddftrain_FD004 /share/tedsds/rf2_model_FD004
